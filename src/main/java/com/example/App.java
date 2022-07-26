67ad8ef1c9e33c1453ffa30734a66c5e17d5730c
Emme <menezes.ecd@gmail.com>
2022-07-26 17:34:54 -0300

    First commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import java.io.FileWriter;
+import java.nio.channels.WritableByteChannel;
+import java.io.FileReader;
+import java.util.List;
+import com.opencsv.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        readAllDataAtOnce(args[0], args[1]);
+        System.err.println("Conversao concluida");
+    }
+
+    public static void readAllDataAtOnce(String inputFile, String outputFile)
+    {
+        try {
+            FileReader filereader = new FileReader(inputFile);    
+            CSVReader csvReader = new CSVReaderBuilder(filereader)
+                                    .build();
+            List<String[]> csvData = csvReader.readAll();
+            
+            FileWriter xmlData = new FileWriter(outputFile);
+            
+            String[] header = csvData.remove(0);
+            
+            int index;
+
+            xmlData.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
+            xmlData.write("<root>\n");
+            for (String[] row : csvData) {
+                index = 0;
+                xmlData.write("\t<row>\n");
+                for (String cell : row) {
+                    xmlData.write("\t\t<" + header[index] + ">");
+                    xmlData.write(cell);
+                    xmlData.write("</" + header[index] + ">\n");
+                    index++;
+                }
+                xmlData.write("\t</row>\n");
+            }           
+            xmlData.write("</root>\n");
+            xmlData.close();
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
